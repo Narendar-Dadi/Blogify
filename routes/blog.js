588cc11d2d1679d7ba7399c22c14c88b10811545
Narendar <dadinarendar6@gmail.com>
2025-10-20 11:00:45 +0530

    changed the blob settings in vercel

--- routes/blog.js
+++ routes/blog.js
@@ -1,24 +1,14 @@
 const { Router } = require("express");
 const multer = require("multer");
-const path = require("path");
+const { put } = require("@vercel/blob"); // 1. Import Vercel Blob
 const router = Router();
 
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
-// ...
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads/`)); // <-- THIS WILL NOT WORK ON VERCEL
-  },
-  filename: function (req, file, cb) {
-    const fileName = `${Date.now()}-${file.originalname}`;
-    cb(null, fileName);
-  },
-});
-
-const upload = multer({ storage: storage }); // <-- THIS USES DISK STORAGE
-// ...
+// 2. Use memoryStorage to hold the file as a buffer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
@@ -32,14 +22,15 @@ router.get("/:id", async (req, res) => {
     "createdBy"
   );
   console.log("Blog", blog);
-  console.log("comments",comments);
-  
+  console.log("comments", comments);
+
   return res.render("blog", {
     user: req.user,
     blog,
     comments,
   });
 });
+
 router.post("/comment/:blogId", async (req, res) => {
   await Comment.create({
     content: req.body.content,
@@ -47,18 +38,52 @@ router.post("/comment/:blogId", async (req, res) => {
     createdBy: req.user._id,
   });
 
-  return res.redirect(`/blog/${req.params.blogId}`)
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
+// 3. This route is now completely changed to support Vercel Blob
 router.post("/", upload.single("coverImage"), async (req, res) => {
-  const { title, body } = req.body;
-  const blog = await Blog.create({
-    title,
-    body,
-    createdBy: req.user._id,
-    coverImageURL: `/uploads/${req.file.filename}`,
-  });
-  return res.redirect(`/blog/${blog._id}`);
+  try {
+    const { title, body } = req.body;
+
+    // Get the file from memory
+    const file = req.file;
+    if (!file) {
+      return res.status(400).render("addBlog", {
+        user: req.user,
+        error: "Please upload a cover image.",
+      });
+    }
+
+    // Create a unique filename
+    const filename = `${Date.now()}-${file.originalname}`;
+
+    // Upload the file buffer to Vercel Blob
+    const { url } = await put(
+      filename, // The filename
+      file.buffer, // The file data from memory
+      {
+        access: "public", // Make it publicly accessible
+        token: process.env.BLOB_READ_WRITE_TOKEN, // Pass the token from env variables
+      }
+    );
+
+    // Create the blog with the new Vercel Blob URL
+    const blog = await Blog.create({
+      title,
+      body,
+      createdBy: req.user._id,
+      coverImageURL: url, // 4. Use the new URL from Vercel Blob
+    });
+
+    return res.redirect(`/blog/${blog._id}`);
+  } catch (error) {
+    console.error("Error during blog creation:", error);
+    return res.render("addBlog", {
+      user: req.user,
+      error: "An error occurred while creating the blog.",
+    });
+  }
 });
 
 module.exports = router;
